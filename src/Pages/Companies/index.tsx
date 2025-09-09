811d99183c0e287b6231c4a6e5011b8a47dbfceb
Nayan Bagale <nvbagale@gmail.com>
2025-09-10 01:12:49 +0530

    feat: add ListOfCompanies component to display company data in a table

--- src/Pages/Companies/index.tsx
+++ src/Pages/Companies/index.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/layout/header"
+import { ListOfCompanies } from "@/components/list-of-companies-table"
 
 const Companies = () => {
   return (
     <div>
       <Header title="List of companies" />
+      <ListOfCompanies/>
     </div>
   )
 }
